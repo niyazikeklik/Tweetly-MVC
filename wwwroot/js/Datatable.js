8b87fb752853781e2f94d802f8228fc82b653bfc
Niyazi Keklik <niyazikeklik@gmail.com>
2021-09-23 20:04:10 +0300

    Grafikler Eklendi (Beta), Login algoritması güncellendi

---
+++ wwwroot/js/Datatable.js
@@ -0,0 +1,98 @@
+﻿var table;
+async function tableAyarla() {
+    console.log(1);
+    table = await $('.fl-table').DataTable({
+        paging: true,
+        lengthMenu: [
+            [10, 25, 50, 100, 200, 500, -1],
+            ['10 Satır', '25 Satır', '50 Satır', '100 Satır', '200 Satır', '500 Satır', 'Hepsini Göster']
+        ],
+        info: false,
+        ordering: true,
+        searching: true,
+        fixedHeader: true,
+        responsive: true,
+        colReorder: true,
+        select: true,
+        //columnDefs: [{
+        //    orderable: false,
+        //    className: 'select-checkbox',
+        //    targets: 0
+        //}],
+        select: {
+            style: 'multi',
+            blurable: true
+            //  selector: 'td:first-child'
+        },
+        lengthChange: false,
+        buttons: [
+            {
+                extend: 'pageLength',
+                text: 'Satır Sayısı',
+                className: 'tablobutonlar'
+            },
+            {
+
+                extend: 'searchBuilder',
+                text: 'Gelişmiş Arama',
+                className: 'tablobutonlar',
+                enterSearch: true,
+
+            },
+            {
+                extend: 'colvis',
+                text: 'Sütun Gizle',
+                collectionLayout: 'two-column',
+                className: 'tablobutonlar'
+            },
+            {
+                extend: 'selectAll',
+                text: 'Tümünü seç',
+                className: 'tablobutonlar'
+            },
+            /*,
+            {
+                extend: 'collection',
+                text: 'Diğerleri',
+                className: 'tablobutonlar',
+                buttons: [
+                    {
+                        collectionTitle: 'Visibility control',
+                        extend: 'colvis',
+                        collectionLayout: 'two-column',
+                        className: 'tablobutonlar'
+                    }
+                ]
+            }*/
+
+        ]
+
+    });
+    table.buttons().container().appendTo($('div.column.is-one-half', table.table().container()).eq(0));
+    $('.dt-buttons.field.is-grouped').append(`<button id="TakipCik" class ="button is-light buttons-collection tablobutonlar"">Seçilenleri Takipten Çık</button>`);
+    $('.dt-buttons.field.is-grouped').append(`<button id="Yenile" asp-area="" asp-controller="Home" asp-action="Yenile" class ="button is-light buttons-collection tablobutonlar"">Yenile</button>`);
+    $('#DataTables_Table_0_filter input').attr("placeholder", "Ara");
+    $('#DataTables_Table_0_filter').css("text-align", "left");
+    // $('#DataTables_Table_0_filter').html($('#DataTables_Table_0_filter input'));
+
+
+    $("#TakipCik").on("click", function () {
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/TakipCik",
+            data: { Usernames: $("tr.selected #username").text() },
+            dataType: "text",
+            success: function (msg) {
+                // $("tr.selected").css("background-color", "#dc3545");
+                //$("tr.selected").css("color", "white");
+                $('tr.selected button[name="Usernames"]').text("Takip et");
+                $('tr.selected').remove();
+            }
+        });
+
+    });
+
+}
+
+tableAyarla();
---
+++ wwwroot/js/Grafikler.js
@@ -0,0 +1,76 @@
+﻿function createCinsiyetGrafik(erkekSayisi,kadinSayisi,unisex,belirsiz) {
+    const data = {
+        labels: [
+            'Kız',
+            'Erkek',
+            'Unisex',
+            'Belirsiz',
+        ],
+        datasets: [
+            {
+                label: 'Cinsiyet Dağılımı',
+                backgroundColor: [
+                    'rgba(255, 50, 50, 0.6)',
+                    'rgba(50, 50, 255, 0.6)',
+                    'rgba(50, 255, 50, 0.6)',
+                    'rgba(75, 75, 75, 0.6)'
+                ],
+                borderColor: [
+                    'rgba(255, 100, 100, 0.6)'
+                ],
+                data: [erkekSayisi, kadinSayisi, unisex, belirsiz],
+            }]
+    };
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {}
+    };
+    var myChart = new Chart(document.getElementById('myChart'), config);
+    myChart.canvas.parentNode.style.height = '512px';
+    myChart.canvas.parentNode.style.width = '512px';
+}
+
+function createTakipciDurumuGrafik(begeniAktiflik, tweetAktiflik, OrtTweet, OrtBegeni, GTK) {
+    const data2 = {
+        labels: ["Beğeni Aktifliği", "Tweet Aktifliği", "Ortalama Tweet (Günde)", "Ortalama Beğeni (Günde)", "Genl Takipçi Kalitesi"],
+        datasets: [{
+            label: 'Takipçi Davranışları',
+            data: [begeniAktiflik, tweetAktiflik, OrtTweet, OrtBegeni, GTK],
+            backgroundColor: [
+                'rgba(255, 50, 60, 0.6)',
+                'rgba(50, 200, 50, 0.6)',
+                'rgba(150, 100, 27, 0.6)',
+                'rgba(35, 100, 255, 0.6)',
+                'rgba(200, 50, 230, 0.6)'
+            ],
+            borderColor: [
+                'rgba(255, 0, 0, 0.6)',
+            ],
+            borderWidth: 2
+        }]
+    };
+    const config2 = {
+        type: 'bar',
+        data: data2,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    };
+    var myChart2 = new Chart(document.getElementById('myChart2'), config2);
+    myChart2.canvas.parentNode.style.height = '512px';
+    myChart2.canvas.parentNode.style.width = '256px';
+}
+
+function grafiklerToggle() {
+    $("#grafikler").toggle();
+}
+
+
+createCinsiyetGrafik($("#kadin").val(), $("#erkek").val(), $("#unisex").val(), $("#belirsiz").val());
+createTakipciDurumuGrafik($("#toplamSonBegeni").val(), $("#toplamSonTweet").val(), $("#toplamOrtTweet").val(), $("#toplamOrtBegeni").val(), $("#genelTakipciKalitesi").val());
+//createTakipciDurumuGrafik(10, 20, 30, 40, 50, 55);
